4e3b7d16725218c52a2d7d2cf3e9648348a2c1b1
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-08-18 17:02:30 -0300

    https

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const express = require('express')
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT;
+const port = process.env.PORT || 8001;
 const cors = require('cors');
 app.use(express.json());
 app.use(cors());
@@ -15,6 +17,13 @@ app.use('/users', users);
 app.use('/edificio', edificio);
 app.use('/uploads', uploads);
 
-app.listen(port, () =>  {
-    console.log(`Estamos trabajando en el puerto ${port}`);
+const options = {
+  key: fs.readFileSync('/home/serpaadministrador.com.ar/ssl.key'),
+  cert: fs.readFileSync('/home/serpaadministrador.com.ar/ssl.cert')
+};
+
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
+  console.log(`Estamos trabajando en el puerto ${port} en modo seguro HTTPS`);
 });
